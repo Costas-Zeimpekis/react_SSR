4d90c2b98ef5027b941e8c2a4497f63bb5fbe8c3
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-08-25 10:51:34 +0300

    Added Routes

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import renderer from './helpers/renderer';
 const app = express();
 
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.send(renderer());
+app.get('*', (req, res) => {
+  res.send(renderer(req));
 });
 
 app.listen(3000, () => console.log('Listening to port 3000'));
