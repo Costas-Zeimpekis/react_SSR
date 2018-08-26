47d2cb423cbd27373310c949c9725c6347da9d0c
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-08-26 15:29:28 +0300

    Addedd oath

--- src/index.js
+++ src/index.js
@@ -11,20 +11,21 @@ import Routes from './client/Routes';
 import createStore from './helpers/createStore';
 import renderer from './helpers/renderer';
 import proxy from 'express-http-proxy';
+
 const app = express();
 
 app.use(
   '/api',
   proxy('http://react-ssr-api.herokuapp.com', {
     proxyReqOptDecorator(opts) {
-      opts.header['x-forwarded-host'] = 'localhost:300';
+      opts.headers['x-forwarded-host'] = 'localhost:3000';
       return opts;
     }
   })
 );
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-  const store = createStore();
+  const store = createStore(req);
 
   const promises = matchRoutes(Routes, req.path).map(({ route }) => {
     return route.loadData ? route.loadData(store) : null;
